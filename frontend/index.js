d98cb7e8e7e07cd85cbecf70414e7ad5e930fac5
mhavelant <mhavelant@brainsum.com>
2018-07-31 15:11:11 +0200

    Add "/api/v1/test/start" endpoint that returns a random dummy backstop.json file as json.
    	- Also update Dockerfile to use the alpine base image and tini
    	- Add nodemon as a dev dep

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +10,24 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from frontend, again!'});
+app.get('/', function (req, res) {
+  res.json({ message: 'General Kenobi.'});
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.post('/api/v1/test/start', function (req, res) {
+    const testId = Math.floor(Math.random() * 7) + 1;
+    const backstopConfig = JSON.parse(fs.readFileSync(path.join(__dirname, 'tmp', 'backstop.' + testId + '.json'), 'utf8'));
+    return res.json(backstopConfig);
+
+    /*
+        Query an idle test.
+        Set it to "in progress".
+        Return the test data.
+
+        @todo: What if there are no tests?
+     */
+});
+
+app.listen(PORT, HOST, function () {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
