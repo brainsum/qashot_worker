b4bb96ae7c515a62327b64359432d266def19807
mhavelant <mhavelant@brainsum.com>
2018-08-02 17:59:45 +0200

    Try to exit apps as gracefully as possible.

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 const amqp = require('amqplib');
+const util = require('util');
 
 // Constants
 const PORT = 8080;
@@ -113,6 +114,29 @@ app.post('/api/v1/test/start', function (req, res) {
      */
 });
 
-app.listen(PORT, HOST, function () {
+const server = app.listen(PORT, HOST, function () {
     console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+const signals = {
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+    console.log("shutdown!");
+    server.close(() => {
+        console.log(`server stopped by ${signal} with value ${value}`);
+        process.exit(128 + value);
+    });
+};
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+        console.log(`process received a ${signal} signal`);
+        shutdown(signal, signals[signal]);
+    });
 });
