82ee486ab6e73d1f97d74dc2d47a11bdc9f310ed
mhavelant <mhavelant@brainsum.com>
2018-08-02 16:37:28 +0200

    Add RabbitMQ (frontend writes, workers read), Replace HAProxy with traefik.

--- backstopjs_worker/index.js
+++ backstopjs_worker/index.js
@@ -7,13 +7,209 @@ const path = require('path');
 const fs = require('fs');
 const backstop = require('backstopjs');
 const util = require('util');
+const url = require('url');
+const amqp = require('amqplib');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const routingKey = "tests";
+const exchangeName = "backstop-worker";
+const qName = "backstop-puppeteer";
 
-// App
-const app = express();
+let rabbitConnection = undefined;
+
+function executeReference(config) {
+    return backstop('reference', { config: config })
+        .then(function () {
+            console.error(`Reference success for test ${config['id']}.`);
+        })
+        .catch(function () {
+            console.error(`Reference fail for test ${config['id']}.`);
+        });
+}
+
+function executeTest(config) {
+    return backstop('test', { config: config })
+        .then(function () {
+            console.log(`Test success for test ${config['id']}.`);
+        })
+        .catch(function () {
+            console.log(`Test fail for test ${config['id']}.`);
+        });
+}
+
+function startTest(config) {
+    console.log(util.inspect(config));
+
+    return executeReference(config)
+        .then(function () {
+            return executeTest(config);
+        });
+}
+
+function ensureDirectory(path) {
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, 0o775);
+    }
+}
+
+function delay(t, v) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, v), t)
+    });
+}
+
+function rabbitTestLoop() {
+    console.time('rabbitTestLoop');
+
+    rabbitRead()
+        .then(data => {
+            const backstopConfig = data;
+            console.log('Data received. Config ID is: ' + backstopConfig['id']);
+
+            const templates = path.join(__dirname, 'templates');
+            const runtime = path.join(__dirname, 'runtime', backstopConfig['id']);
+
+            ensureDirectory(runtime);
+
+            // Engine options:
+            // - puppeteer, chromy
+            // - slimerjs
+            // - casper
+            backstopConfig['paths'] = {
+                "engine_scripts": path.join(templates, "puppeteer_scripts"),
+                "bitmaps_reference": path.join(runtime, "reference"),
+                "bitmaps_test": path.join(runtime, "test"),
+                "html_report": path.join(runtime, "html_report"),
+                "ci_report": path.join(runtime, "ci_report")
+            };
+
+            backstopConfig['engineOptions'] = {
+                'ignoreHTTPSErrors': false,
+                'args': [
+                    '--no-sandbox',
+                    // '--disable-setuid-sandbox',
+                ],
+            };
+
+            // backstopConfig['debug'] = true;
+            // backstopConfig['debugWindow'] = true;
+
+            Object.keys(backstopConfig['paths']).forEach(function (key) {
+                ensureDirectory(backstopConfig['paths'][key]);
+            });
+
+            startTest(backstopConfig)
+                .finally(function () {
+                    console.timeEnd('testLoop');
+                    console.log(`Test ${backstopConfig['id']} ended.`);
+                    rabbitTestLoop();
+                });
+        })
+        .catch(error => {
+            console.log('Error while reading from RabbitMQ');
+            console.log(util.inspect(error));
+            const timeout = 10000;
+            return delay(timeout).then(() => {
+                rabbitTestLoop();
+            });
+        });
+
+}
+
+function rabbitSetup() {
+    rabbitConnection.then(conn => {
+        return conn.createChannel();
+    })
+        .then(ch => {
+            return ch.
+            assertExchange(exchangeName, 'direct', { durable: true })
+                .then(() => {
+                    return ch.assertQueue(qName, { exclusive: false });
+                })
+                .then(q => {
+                    return ch.bindQueue(q.queue, exchangeName, routingKey);
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            throw new Error(err.message);
+        });
+}
+
+function connect() {
+    const connectionString = process.env.COMPOSE_RABBITMQ_URL;
+
+    if (connectionString === undefined) {
+        console.error("Please set the COMPOSE_RABBITMQ_URL environment variable");
+        throw new Error("Please set the COMPOSE_RABBITMQ_URL environment variable");
+    }
+
+    const parsedurl = url.parse(connectionString);
+    const connection = amqp.connect(connectionString, { servername: parsedurl.hostname });
+    connection.then(() => {
+        console.log('Connection to RabbitMQ has been established.');
+        rabbitConnection = connection;
+        rabbitSetup();
+        rabbitTestLoop();
+        return null;
+    })
+        .catch((error) => {
+            const timeout = 3000;
+
+            console.log(`Connection to RabbitMQ failed. Retry in ${timeout / 1000} seconds..`);
+            // console.log(util.inspect(error));
+            delay(timeout).then(() => {
+                connect();
+            });
+        });
+}
+
+connect();
+
+// function rabbitWrite(message) {
+//     return rabbitConnection
+//         .then(conn => {
+//             conn.createChannel();
+//         })
+//         .then(ch => {
+//             ch.publish(exchangeName, routingKey, Buffer.from(message));
+//             const msgTxt = message + " : Message sent at " + new Date();
+//             console.log(" [+] %s", msgTxt);
+//             return new Promise(resolve => {
+//                 resolve(message);
+//             });
+//         });
+// }
+
+function rabbitRead() {
+    return rabbitConnection
+        .then(conn => {
+            return conn.createChannel();
+        })
+        .then(ch => {
+            return ch.get(qName, {}).then(msgOrFalse => {
+                // let result = "No messages in queue";
+                // if (msgOrFalse !== false) {
+                //     result = `${msgOrFalse.content.toString()} : Message received at ${new Date()}`;
+                //     ch.ack(msgOrFalse);
+                // }
+                // console.log("Reading from RabbitMQ:: [-] %s", result);
+
+                return new Promise((resolve, reject) => {
+                    if (msgOrFalse !== false) {
+                        console.log("Reading from RabbitMQ:: [-] %s", `${msgOrFalse.content.toString()} : Message received at ${new Date()}`);
+                        ch.ack(msgOrFalse);
+                        resolve(JSON.parse(msgOrFalse.content.toString()));
+                    }
+                    else {
+                        reject('No messages in queue.');
+                    }
+                });
+            });
+        });
+}
 
 function loadDockerId() {
     const childProcess = require('child_process');
@@ -52,6 +248,8 @@ const workerConfig = loadWorkerConfig();
 console.log('Worker config should be loaded. See:');
 console.log(util.inspect(workerConfig));
 
+// App
+const app = express();
 app.get('/', (req, res) => {
     client.get('http://frontend:8080/', (response) => {
         let data = '';
@@ -77,117 +275,3 @@ app.get('/hello', (req, res) => {
 app.listen(PORT, HOST, function () {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
-
-function executeReference(config) {
-    return backstop('reference', { config: config })
-        .then(function () {
-            console.error(`Reference success for test ${config['id']}.`);
-        })
-        .catch(function () {
-            console.error(`Reference fail for test ${config['id']}.`);
-        });
-}
-
-function executeTest(config) {
-    return backstop('test', { config: config })
-        .then(function () {
-            console.log(`Test success for test ${config['id']}.`);
-        })
-        .catch(function () {
-            console.log(`Test fail for test ${config['id']}.`);
-        });
-}
-
-function startTest(config) {
-    console.log(util.inspect(config));
-
-    return executeReference(config)
-        .then(function () {
-            return executeTest(config);
-        });
-}
-
-function ensureDirectory(path) {
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path, 0o775);
-    }
-}
-
-function testLoop() {
-    /** @todo
-     * Clean up code (e.g use "request" module, maybe it's cleaner)
-     * Add JWT authentication to the request
-     * Add recursive call
-     */
-
-    console.time('testLoop');
-    const payload = JSON.stringify({});
-    const options = {
-        hostname: 'frontend',
-        port: 8080,
-        path: '/api/v1/test/start',
-        method: 'POST'
-    };
-    const request = client.request(options, function (response) {
-        let data = '';
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        response.on('end', () => {
-            const backstopConfig = JSON.parse(data);
-
-            console.log('Data received. Config ID is: ' + backstopConfig['id']);
-
-            const templates = path.join(__dirname, 'templates');
-            const runtime = path.join(__dirname, 'runtime', backstopConfig['id']);
-
-            ensureDirectory(runtime);
-
-            // Engine options:
-            // - puppeteer, chromy
-            // - slimerjs
-            // - casper
-            backstopConfig['paths'] = {
-                "engine_scripts": path.join(templates, "puppeteer_scripts"),
-                "bitmaps_reference": path.join(runtime, "reference"),
-                "bitmaps_test": path.join(runtime, "test"),
-                "html_report": path.join(runtime, "html_report"),
-                "ci_report": path.join(runtime, "ci_report")
-            };
-
-            backstopConfig['engineOptions'] = {
-                'ignoreHTTPSErrors': false,
-                'args': [
-                    '--no-sandbox',
-                    // '--disable-setuid-sandbox',
-                ],
-            };
-
-
-
-            // backstopConfig['debug'] = true;
-            // backstopConfig['debugWindow'] = true;
-
-            Object.keys(backstopConfig['paths']).forEach(function (key) {
-                ensureDirectory(backstopConfig['paths'][key]);
-            });
-
-            startTest(backstopConfig)
-                .finally(function () {
-                    console.timeEnd('testLoop');
-                    console.log(`Test ${backstopConfig['id']} ended.`);
-                    // testLoop();
-                });
-        });
-    });
-
-    request.on('error', function (error) {
-        console.error(`Requesting a new test failed. Error: ${error.message}`);
-    });
-
-    request.write(payload);
-    request.end();
-}
-
-testLoop();
--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,94 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
+const amqp = require('amqplib');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const routingKey = "tests";
+const exchangeName = "backstop-worker";
+const qName = "backstop-puppeteer";
+
+function delay(t, v) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, v), t)
+    });
+}
+
+function rabbitWrite(message) {
+    return rabbitConnection
+        .then(conn => {
+            return conn.createChannel();
+        })
+        .then(ch => {
+            ch.publish(exchangeName, routingKey, Buffer.from(message));
+            const msgTxt = message + " : Message sent at " + new Date();
+            console.log(" [+] %s", msgTxt);
+            return new Promise(resolve => {
+                resolve(message);
+            });
+        });
+}
+
+function pushToRabbit() {
+    for (let testId = 1; testId < 7; ++testId) {
+        const backstopConfig = JSON.stringify(JSON.parse(fs.readFileSync(path.join(__dirname, 'tmp', 'backstop.' + testId + '.json'), 'utf8')));
+        rabbitWrite(backstopConfig);
+    }
+}
+
+function rabbitSetup() {
+    rabbitConnection.then(conn => {
+        return conn.createChannel();
+    })
+        .then(ch => {
+            return ch.
+            assertExchange(exchangeName, 'direct', { durable: true })
+                .then(() => {
+                    return ch.assertQueue(qName, { exclusive: false });
+                })
+                .then(q => {
+                    return ch.bindQueue(q.queue, exchangeName, routingKey);
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            throw new Error(err.message);
+        });
+}
+
+let rabbitConnection = undefined;
+function connect() {
+    const connectionString = process.env.COMPOSE_RABBITMQ_URL;
+
+    if (connectionString === undefined) {
+        console.error("Please set the COMPOSE_RABBITMQ_URL environment variable");
+        throw new Error("Please set the COMPOSE_RABBITMQ_URL environment variable");
+    }
+
+    const parsedurl = url.parse(connectionString);
+    const connection = amqp.connect(connectionString, { servername: parsedurl.hostname });
+    connection.then(() => {
+        console.log('Connection to RabbitMQ has been established.');
+        rabbitConnection = connection;
+        rabbitSetup();
+        pushToRabbit();
+        return null;
+    })
+        .catch((error) => {
+            const timeout = 3000;
+
+            console.log(`Connection to RabbitMQ failed. Retry in ${timeout / 1000} seconds ..`);
+            // console.log(util.inspect(error));
+            delay(timeout).then(() => {
+                connect();
+            });
+        });
+}
+
+connect();
 
 // App
 const app = express();
