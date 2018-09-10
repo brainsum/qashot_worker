894892c8a6ce0cf55a59575a3f84375e2b21d2f8
mhavelant <mhavelant@brainsum.com>
2018-09-10 13:42:23 +0200

    Add fetch endpoint, update add endpoint so it stores the test UUID.

--- result_queue/src/route/api/v1/result.js
+++ result_queue/src/route/api/v1/result.js
@@ -11,18 +11,91 @@ const db = require('../../../database');
  */
 const ResultModel = db.models.Result;
 
+router.post('/fetch', asyncHandler(async function fetchResultsHandler(req, res, next) {
+    if ('undefined' === typeof req.body) {
+        return res.status(400).json({
+            message: 'The request body is empty.'
+        });
+    }
+
+    const origin = req.body.origin;
+    if ('undefined' === typeof origin) {
+        return res.status(400).json({
+            message: 'The origin field is required and cannot be empty.'
+        });
+    }
+
+    const testUuids = req.body.testUuids;
+    if ('undefined' === typeof testUuids || !Array.isArray(testUuids) || testUuids.length === 0) {
+        return res.status(400).json({
+            message: 'The testUuids field is required and cannot be empty.'
+        });
+    }
+
+    let results = undefined;
+    try {
+        results = await ResultModel.findAll({
+            // @todo: createdAt : Fetch only the latest for each UUID?
+            where: {
+                "rawData.original_request.origin": origin,
+                testUuid: {
+                    [db.Op.in]: testUuids
+                }
+            },
+            limit: 20
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: error
+        });
+    }
+
+    let rowsKeyed = {};
+    results.forEach(async function (result) {
+        rowsKeyed[result.uuid] = {
+            createdAt: result.createdAt,
+            sentAt: new Date(),
+            data: result.rawData
+        };
+    });
+
+    try {
+        let update = ResultModel.update({
+            status: 'ok',
+            statusMessage: `Fetched by consumer (${origin}).`,
+            sentAt: new Date()
+        }, {
+            where: {
+                uuid: {
+                    [db.Op.in]: Object.keys(rowsKeyed)
+                }
+            }
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return res.status(200).json({
+        results: rowsKeyed
+    });
+}));
+
 router.post('/add', asyncHandler(async function addResultHandler(req, res, next) {
     const body = req.body;
     const uuid = body.uuid;
 
     const resultData = {
         uuid: uuid,
+        testUuid: body.original_request.uuid,
         rawData: body
     };
 
     let storedResult = undefined;
     try {
-        storedResult = ResultModel.create(resultData);
+        storedResult = await ResultModel.create(resultData);
     }
     catch (error) {
         console.error(error);
