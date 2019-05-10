fd385a9d06f35199a68894cd3d65255d5eda9494
mhavelant <mhavelant@brainsum.com>
2019-05-10 15:38:06 +0200

    QAP-000 | Add helper script for npm outdated checks.

---
+++ check-npm-updates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+services=('frontend' 'backstopjs_worker' 'result_queue')
+
+for service in ${services[@]}
+do
+  echo "NPM status for ${service}:"
+  cd "${service}" && npm outdated -l
+  cd ..
+  echo ""
+done
