7fc67f19f11ffc1217fa631633499589406cce05
mhavelant <mhavelant@brainsum.com>
2019-05-10 15:34:44 +0200

    QAP-000 | Optimize chrome CLI params.

--- backstopjs_worker/config/worker/chrome.js
+++ backstopjs_worker/config/worker/chrome.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
     "browser": "chrome",
@@ -10,14 +10,27 @@ module.exports = {
             "waitTimeout": 20000,
             "ignoreHTTPSErrors": true,
             "args": [
-                // '--no-sandbox',
-                // '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--headless',
-                '--disable-gpu',
-                '--ignore-certificate-errors',
-                '--force-device-scale-factor=1',
-                '--disable-infobars=true'
+                // "--no-sandbox",
+                // "--disable-setuid-sandbox",
+                // "--no-zygote",
+                "--disable-dev-shm-usage",
+                "--lang=en-GB,en-US",
+                "--headless",
+                "--disable-gpu",
+                "--ignore-certificate-errors",
+                "--force-device-scale-factor=1",
+                "--disable-infobars=true",
+                "--process-per-site",
+                "--disable-accelerated-2d-canvas",
+                "--disable-accelerated-jpeg-decoding",
+                "--disable-accelerated-mjpeg-decode",
+                "--disable-accelerated-video-decode",
+                "--disable-gpu-rasterization",
+                "--disable-zero-copy",
+                "--disable-extensions",
+                "--disable-notifications",
+                "--disable-sync",
+                "--mute-audio"
             ]
         }
     }
