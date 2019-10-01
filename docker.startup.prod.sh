88f23d1883bdcc59dd453868150260fec8e49c23
mhavelant <mhavelant@brainsum.com>
2019-10-01 15:19:23 +0200

    QAP-000 | Use dash instead of dot for pihole host, downscale workers.

--- docker.startup.prod.sh
+++ docker.startup.prod.sh
@@ -2,11 +2,11 @@
 
 COMPOSE_FILES="-f docker-compose.yml"
 COMPOSE_MISC_OPTIONS="-d --remove-orphans"
-COMPOSE_SCALE_CHROME="--scale backstopjs_worker_chrome=3"
-COMPOSE_SCALE_FIREFOX="--scale backstopjs_worker_firefox=3"
+COMPOSE_SCALE_CHROME="--scale backstopjs_worker_chrome=1"
+COMPOSE_SCALE_FIREFOX="--scale backstopjs_worker_firefox=1"
 
 docker-compose ${COMPOSE_FILES} up ${COMPOSE_MISC_OPTIONS} ${COMPOSE_SCALE_CHROME} ${COMPOSE_SCALE_FIREFOX}
 
 echo "Waiting a bit for services to start up.."
-sleep 4
+sleep 5
 docker-compose ${COMPOSE_FILES} ps
